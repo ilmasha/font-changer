4e5dbec47df1c577bc1c399ca75629ae0a741c24
ilmasha <illmaria257@gmail.com>
2024-11-27 14:01:10 +0300

    создали функцию для изменения шрифтов и тд

--- functions.py
+++ functions.py
@@ -1,13 +1,24 @@
-## functions модуль для импорта функций для работы
+import os
+from docx import Document
 
-#TODO - import
+def update_document_format(file_path):
+    """Изменяет формат документа: шрифт, размер шрифта и межстрочный интервал."""
+    try:
+        doc = Document(file_path)  # Открываем документ .docx  b
+        print(f"Изменяем формат документа: {file_path}")
+        
+        # Изменение шрифта и размера шрифта
+        for para in doc.paragraphs:
+            for run in para.runs:
+                run.font.name = 'Times New Roman'  # Меняем шрифт на Times New Roman
+                run.font.size = 14  # Устанавливаем размер шрифта 14
+            para.paragraph_format.line_spacing = 1.5  # Устанавливаем межстрочный интервал
+        
+        doc.save(file_path)  # Сохраняем документ с изменениями
+        print(f"Документ {file_path} успешно сохранён с изменениями.")
+    except Exception as e:
+        print(f"Ошибка при обработке документа {file_path}: {e}")
 
-
-def func(n):
-    ""Функция 1""
-
-
-
-def func2(n):
-     ""Функция 2""
+def process_documents(source_folder):
+    """Обрабатывает все документы .docx в папке."""
     pass
